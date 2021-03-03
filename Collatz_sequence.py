445065060164cfe24be1a1d7d5f0e084e1ef884d
Josh <j.j.h.lindner@gmail.com>
2021-03-03 11:34:18 -0800

    A simple program that calculates and prints a Collatz Collatz_sequence for a positive integer.

---
+++ Collatz_sequence.py
@@ -0,0 +1,21 @@
+
+def collatz(number):
+    Bruce= True
+    while Bruce:
+        if number%2==0:
+            number=number//2
+            print(number)
+
+        else:
+            number = (number*3)+1
+            print(number)
+
+        if number==1:
+            Bruce = False
+
+        else:
+            continue
+
+
+
+collatz(int(input("Please input a number")))
